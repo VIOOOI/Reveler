2a2fe674147506ab5ff276662682f5ca3090e038
Vladimir Below <laitvladimir@yandex.ru>
2023-04-24 02:16:28 +0300

    F: RollupError: Could not resolve "./Editor.utils" from "src/components/molecules/editor/useEditorResize.ts"
    Signed-off-by: Vladimir Below <laitvladimir@yandex.ru>

--- src/components/molecules/editor/useEditorResize.ts
+++ src/components/molecules/editor/useEditorResize.ts
@@ -1,7 +1,36 @@
+import { Accessor, createEffect, onCleanup, Setter } from "solid-js";
 
-import { createEffect } from "solid-js";
+type Props = {
+	event: MouseEvent,
+	editorWidth: Accessor<string>,
+	setEditorWidth: Setter<string>,
+}
 
-import { handleMouseDown } from "./Editor.utils";
+export const handleMouseDown = ({ event, editorWidth, setEditorWidth }: Props) => {
+	event.preventDefault();
+
+	const initialX = event.clientX;
+	const initialWidth = parseFloat(editorWidth());
+
+	const handleMouseMove = (event: MouseEvent) => {
+		const deltaX = event.clientX - initialX;
+		const newWidth = initialWidth + deltaX;
+		setEditorWidth(`${Math.max(Math.min(newWidth, window.innerWidth - 100), 400)}px`);
+	};
+
+	const handleMouseUp = () => {
+		document.removeEventListener("mousemove", handleMouseMove);
+		document.removeEventListener("mouseup", handleMouseUp);
+	};
+
+	document.addEventListener("mousemove", handleMouseMove);
+	document.addEventListener("mouseup", handleMouseUp);
+
+	onCleanup(() => {
+		document.removeEventListener("mousemove", handleMouseMove);
+		document.removeEventListener("mouseup", handleMouseUp);
+	});
+};
 
 export const useEditorResize = (
 	editorRef: HTMLDivElement,
